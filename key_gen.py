b195dbc5a23b005de2d647dcd3127a9350ed0b67
krysi <krysiakmaciek1@gmail.com>
2025-04-01 22:28:19 +0200

    Setting const salt and const iv, rename file

--- key_gen.py
+++ key_gen.py
@@ -6,6 +6,10 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.ciphers import Cipher, modes, algorithms
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from pycparser.c_ast import Constant
+
+CONSTANT_SALT = b'\x00' * 16
+CONSTANT_IV = b'\x00' * 16
 
 def gen_key():
     key = rsa.generate_private_key(
@@ -33,18 +37,16 @@ def get_pin():
 def encrypt_pin(pin, key):
     pin_bytes = pin.encode('utf-8')
 
-    salt = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
-        salt=salt,
+        salt=CONSTANT_SALT,
         iterations=100000,
         backend=default_backend()
     )
     aes_key = kdf.derive(pin_bytes)
 
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(CONSTANT_IV), backend=default_backend())
     encryptor = cipher.encryptor()
 
     padder = padding.PKCS7(128).padder()
@@ -52,20 +54,20 @@ def encrypt_pin(pin, key):
 
     encrypted_key = encryptor.update(padded_data) + encryptor.finalize()
 
-    return salt, iv, encrypted_key
+    return encrypted_key
 
-def decrypt_key(encrypted_key, pin, salt, iv):
+def decrypt_key(encrypted_key, pin):
     pin_bytes = pin.encode('utf-8')
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
-        salt=salt,
+        salt=CONSTANT_SALT,
         iterations=100000,
         backend=default_backend()
     )
     aes_key = kdf.derive(pin_bytes)
 
-    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(CONSTANT_IV), backend=default_backend())
     decryptor = cipher.decryptor()
 
     unpadder = padding.PKCS7(128).unpadder()
@@ -82,12 +84,12 @@ def main():
     private_key, public_key = gen_key()
     print("Private Key:", private_key.decode())
     pin = get_pin()
-    salt, iv, encrypted_key = encrypt_pin(pin, private_key)
+    encrypted_key = encrypt_pin(pin, private_key)
     save_key_to_file('E:\\private_key.pem', encrypted_key)
 
     if private_key != encrypted_key:
         print("Key encrypted successfully!")
-    if private_key == decrypt_key(encrypted_key, pin, salt, iv):
+    if private_key == decrypt_key(encrypted_key, pin):
         print("Key decrypted successfully!")
 
 if __name__ == '__main__':
